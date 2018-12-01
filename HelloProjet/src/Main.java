bb64713ea6dd9a4eb7d254f0ea1e690a955e086f
sublun <sublun@126.com>
2018-12-01 16:31:30 +0800

    张三提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("我张三怕过谁！")
+        System.out.println("我张三怕过谁")
     }
 }
